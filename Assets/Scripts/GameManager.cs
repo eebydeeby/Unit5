0883d41bd51459994df6a078849a1b74c116d813
eebydeeby <86206723+eebydeeby@users.noreply.github.com>
2023-11-07 02:20:44 +0000

    Added score system and particle fx

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
     private float spawnRate = 1.0f;
     public List<GameObject> targets;
+    private int score;
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
-    void Start() { StartCoroutine(SpawnTarget()); }
+    void Start() {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score: " + score;
+        UpdateScore(0);
+    }
 
     IEnumerator SpawnTarget() {
         while(true) {
@@ -17,5 +25,10 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+    public void UpdateScore(int scoreToAdd) {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour
-{
+{   
     private Rigidbody targetRb;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     void Start() {
         targetRb = GetComponent<Rigidbody>();
@@ -17,6 +20,7 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(),
         ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     Vector3 RandomForce() {
@@ -31,6 +35,8 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown() {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other) {
